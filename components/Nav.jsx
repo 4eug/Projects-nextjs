e425fa4330fbbcbd9defd4f861b405377e0efe6c
Eugene Debrah <debraheug@gmail.com>
2022-02-18 17:34:26 +0000

    Done with Nav Bar left with Mobile Responsivess

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,11 +1,29 @@
 import React from "react";
+import Image from "next/image";
 
-function Nav() {
+function Nav(props) {
   return (
-    <nav className="h-ijikod-px-60 bg-red-200 flex justify-between items-center px-10">
-      <div>Logo</div>
-      <div>Links</div>
-      <div>Button</div>
+    <nav className="h-ijikod-px-60 bg-white-200 flex justify-between items-center px-60">
+      <div className="h-8 w-20 relative">      
+       <Image 
+       layout="fill"
+       objectFit="contain"
+       priority
+       src={"/Logo.png"}
+       alt="ijikod logo"
+       />
+      
+      </div>
+      <div className="text-black-500 group  inline-flex  items-center md:gap-7 text-base font-medium">
+          <div className="hover:text-gray-400">About us</div>
+          <ol>Services</ol>
+          <ol>Portfolio</ol>
+          <ol>Join Us</ol>
+      </div>
+
+    <button className="bg-red-500 hover:bg-red-700 text-white text-center py-2 px-4 rounded-full">
+        Contact Us
+        </button>
     </nav>
   );
 }
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import Nav from "../components/Nav";
+import HeroSection from "../components/Hero Section/Hero";
 
 export default function Home() {
   return (
@@ -13,7 +14,8 @@ export default function Home() {
 
 	  <Nav/>
 
-      <div className="text-red-200">hello</div>
+		{/* <HeroSection /> */}
+      {/* <div className="text-red-200">hello</div> */}
     </>
   );
 }

b92c4bc423e8e2fee12636820ed9f87911150678
Eugene Debrah <debraheug@gmail.com>
2022-02-19 17:34:11 +0000

    Update

--- components/Hero Section/Hero.jsx
+++ components/Hero Section/Hero.jsx
@@ -7,14 +7,19 @@ function HeroSection() {
         <section className="pt-28 bg-gradient-to-r from-rose-900 via-gray-800 to-rose-900;">
             <div className="text-center">
 
-                <h1 className="text-6xl text-white mt-1  font-bold">Impactful Digital Experiences</h1>
+                <h1 className="text-6xl text-white mt-1  font-bold">
+                    Impactful Digital Experiences
+                    </h1>
 
-                <h3 className="text-red-100 mt-8 md:mt-3 ">
+
+                <div className="space-y-3">
+                <p className="text-xl text-white mt-1 font-light">
                     Lorem ipsum dolor sit amet, consectetur adipisicing elit, 
                     sed do eiusmod tempor incididunt ut 
                     labore et dolore magna aliquat enim ad minim veniam, 
                     quis nostrud exercitation.
-                </h3>
+                </p>
+                </div>
 
             {/*  Pull Down Arrow */}
                 <div>
@@ -25,7 +30,7 @@ function HeroSection() {
              {/* Shape */}
 
             <div>
-               <Shape/>
+               {/* <Shape/> */}
             </div>
 
         </section>
--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,6 +1,23 @@
 import React from "react";
 import Image from "next/image";
 
+const NavItem = props => (
+<li>
+    <a href={props.href}>
+        {props.text}
+    </a>
+</li>
+);
+
+const Button = props => (
+    <button
+        type={props.type}
+        className="bg-red-600 hover:bg-red-500 transition-colors duration-300 text-white text-center py-2 px-4 rounded-full"   
+    >
+        {props.children}
+    </button>
+)
+
 function Nav(props) {
   return (
     <nav className="h-ijikod-px-60 bg-white-200 flex justify-between items-center px-60">
@@ -14,16 +31,24 @@ function Nav(props) {
        />
       
       </div>
-      <div className="text-black-500 group  inline-flex  items-center md:gap-7 text-base font-medium">
+      {/* <div className="text-black-500 group  inline-flex  items-center md:gap-7 text-base font-medium">
           <div className="hover:text-gray-400">About us</div>
           <ol>Services</ol>
           <ol>Portfolio</ol>
           <ol>Join Us</ol>
+      </div> */}
+      <div>
+          <ul className="inline-flex space md:gap-7 text-base font-medium">
+              <NavItem href="/about-us" text="About us"/>
+              <NavItem href="/severice" text="Services"/>
+              <NavItem href="/portfolio" text="Porfolio"/>
+              <NavItem href="/join-us" text="Join Us"/>
+          </ul>
       </div>
 
-    <button className="bg-red-500 hover:bg-red-700 text-white text-center py-2 px-4 rounded-full">
+    <Button type="button" >
         Contact Us
-        </button>
+        </Button>
     </nav>
   );
 }

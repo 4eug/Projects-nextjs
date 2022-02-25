e8b041ba5c7f9dbdeb55ba79279e07c1a50b71ce
Eugene Debrah <debraheug@gmail.com>
2022-02-25 10:43:43 +0000

    update on Portfolio card

--- components/Portfolio Section/Portfolio.jsx
+++ components/Portfolio Section/Portfolio.jsx
@@ -9,16 +9,16 @@ function PortfolioSection() {
             <h1 className="text-black text-left text-ijikod-md">
                 Portfolio</h1>
 
-            <p>Lorem ipsum dolor sit amet, consectetur 
+            <p className="w-1/4 mt-5">Lorem ipsum dolor sit amet, consectetur 
                 adipisicing elit, sed do eiusmod tempor 
                 incididunt ut labore et dolore magna aliqua.
                  Ut enim ad minim veniam, quis nostrud</p>
 
-            <div className="block gap-16">
+            <div className="flex gap-16">
                 <PortfolioCard
                 content={"Design and development of a suite of apps for a Ghana based service management company"
                 }
-                src="/core-value-icon-2.svg"
+                src="/Ayuda.svg"
                 techs="WEB DESIGN . MOBILE APP DESIGN . UI/UX DESIGN . WEB DEVELOPMENT . MOBILE DEVELOPMENT "
                 />
 
--- components/utilities/cards/portfolioCard.jsx
+++ components/utilities/cards/portfolioCard.jsx
@@ -3,25 +3,28 @@ import React from "react";
 
 function PortfolioCard(props) {
   return (
-    <section className="mt-32">
-        <div className="md:px-10 2lg:px-24">
+    <section className="mt-20">
+        <div className="md: 2lg:px-24">
             <div className="card">
                 <div className="w-full sm:w-2/3 h/2/5 sm:h-full px-2 xxsm:px-12 sm:px-7 pt-8 sm:pt-0 py-3">
-               <div className="text-left pt-6 relative">
-                <Image 
+               <div className="pt-6 relative">
+                   <div className="object-left-top">
+                   <Image 
                 layout="fill"
                 objectFit="contain"
                 src={props.src}
                 alt="company-logo"
                 />
-                <p className="text-left mt-20">
+
+                   </div>
+                <p className="mb-2 tracking-tight dark:text-white mt-20">
                     {props.content}
                 </p>
                 <button className="white mt-10">
                     Explore
                 </button>
 
-                <h5 className="mt-10">{props.techs}</h5>
+                <h5 className="mb-2 tracking-tight dark:text-white mt-10">{props.techs}</h5>
                 </div>
             <div>
 

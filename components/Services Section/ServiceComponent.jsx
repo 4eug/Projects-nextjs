df7855c243e2b74fdefa1bb65069b306d5bf45fd
Eugene Debrah <debraheug@gmail.com>
2022-03-02 13:11:53 +0000

    Refactored Service Section

--- components/Services Section/ServiceComponent.jsx
+++ components/Services Section/ServiceComponent.jsx
@@ -3,39 +3,43 @@ import React from "react";
 
 function ServiceComponent(props) {
   return (
-    <div
-      className={`flex ${props.flexDirection ?? 'flex-col'} w-full bg-slate-40 justify-center gap-10
+    <div className="mt-32">
+      <div
+        className={`flex ${
+          props.flexDirection ?? "flex-col"
+        } w-full bg-slate-40 justify-center gap-10
       lg:gap-28 
       lg:h-ijikod-px-450
-      ${props.lgFlexDirection ?? 'lg:flex-row'}
+      ${props.lgFlexDirection ?? "lg:flex-row"}
       
       `}
-    >
-      <div className="w-full flex items-center">
-        <div>
-          <h3 className="text-ijikod-wine-dark-primary">{props.title}</h3>
-          <h5 className="text-ijikod-gray mt-5">{props.description}</h5>
+      >
+        <div className="w-full flex items-center">
+          <div>
+            <h3 className="text-ijikod-wine-dark-primary">{props.title}</h3>
+            <h5 className="text-ijikod-gray mt-5">{props.description}</h5>
+          </div>
         </div>
-      </div>
 
-      <div className="w-full">
-        <div className="h-full w-full relative hidden lg:block">
-          <Image
-            layout="fill"
-            objectFit="cover"
-            priority
-            src={props.src}
-            alt="Web-Image"
-          />
-        </div>
-        <div className="h-ijikod-px-450 w-full relative lg:hidden">
-          <Image
-            layout="fill"
-            objectFit="cover"
-            priority
-            src={props.src}
-            alt="Web-Image"
-          />
+        <div className="w-full">
+          <div className="h-full w-full relative hidden lg:block">
+            <Image
+              layout="fill"
+              objectFit="cover"
+              priority
+              src={props.src}
+              alt="Web-Image"
+            />
+          </div>
+          <div className="h-ijikod-px-450 w-full relative lg:hidden">
+            <Image
+              layout="fill"
+              objectFit="cover"
+              priority
+              src={props.src}
+              alt="Web-Image"
+            />
+          </div>
         </div>
       </div>
     </div>
--- components/Services Section/Services.jsx
+++ components/Services Section/Services.jsx
@@ -2,58 +2,53 @@ import React from "react";
 import ServiceComponent from "./ServiceComponent";
 
 function Services() {
+  const serviceArray = [
+    {
+      title: "Web & Desktop Applications",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Utenim ad minim veniam, quis nostrud exercitation ullamco laborisnisi ut aliquip ex ea commodo consequat. Duis aute irure dolor inreprehenderit in voluptate velit esse cillum dolore eu fugiat",
+      imgSrc: "/webImage.svg",
+      id: 1,
+    },
+    {
+      title: "Mobile Apps",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Utenim ad minim veniam, quis nostrud exercitation ullamco laborisnisi ut aliquip ex ea commodo consequat. Duis aute irure dolor inreprehenderit in voluptate velit esse cillum dolore eu fugiat",
+      imgSrc: "/mobileImage.svg",
+      id: 1,
+      lgFlexDirection: "lg:flex-row-reverse",
+    },
+    {
+      title: "Internet of Things (IoT)",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Utenim ad minim veniam, quis nostrud exercitation ullamco laborisnisi ut aliquip ex ea commodo consequat. Duis aute irure dolor inreprehenderit in voluptate velit esse cillum dolore eu fugiat",
+      imgSrc: "/IoT.png",
+      id: 1,
+    },
+    {
+      title: "UI/UX",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Utenim ad minim veniam, quis nostrud exercitation ullamco laborisnisi ut aliquip ex ea commodo consequat. Duis aute irure dolor inreprehenderit in voluptate velit esse cillum dolore eu fugiat",
+      imgSrc: "/UIUX.svg",
+      id: 1,
+      lgFlexDirection: "lg:flex-row-reverse",
+    },
+  ];
+
   return (
     <div className="mt-20">
       <h2 className="text-center text-ijikod-wine-dark-primary">What We Do</h2>
 
       <div className="mt-32">
-        <ServiceComponent
-          title={"Web & Desktop Applications"}
-          description="Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
-                        eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                        enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                        nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-                        reprehenderit in voluptate velit esse cillum dolore eu fugiat"
-          src={"/webImage.svg"}
-        />
-      </div>
-
-      <div className="mt-32">
-        <ServiceComponent
-          lgFlexDirection="lg:flex-row-reverse"
-          title={"Mobile Apps"}
-          description="Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
-                        eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                        enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                        nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-                        reprehenderit in voluptate velit esse cillum dolore eu fugiat"
-          src={"/mobileImage.svg"}
-        />
-      </div>
-
-      <div className="mt-32">
-        <ServiceComponent
-          title={"Internet of Things (IoT)"}
-          description="Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
-                        eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                        enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                        nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-                        reprehenderit in voluptate velit esse cillum dolore eu fugiat"
-          src={"/IoT.png"}
-        />
-      </div>
-
-      <div className="mt-32">
-        <ServiceComponent
-          lgFlexDirection="lg:flex-row-reverse"
-          title={"UI/UX"}
-          description="Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
-                        eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                        enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                        nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-                        reprehenderit in voluptate velit esse cillum dolore eu fugiat"
-          src={"/UIUX.svg"}
-        />
+        {serviceArray.map((Item) => (
+          <ServiceComponent
+            title={Item.title}
+            description={Item.description}
+            src={Item.imgSrc}
+            lgFlexDirection={Item.lgFlexDirection}
+            key={Item.id}
+          />
+        ))}
       </div>
     </div>
   );

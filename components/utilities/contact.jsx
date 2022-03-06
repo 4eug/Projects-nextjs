48f300cf2eea8e18f5cc75ee0c69a6adc3fe2440
Eugene Debrah <debraheug@gmail.com>
2022-03-06 08:52:08 +0000

    Details on Contact Slider

--- components/utilities/contact.jsx
+++ components/utilities/contact.jsx
@@ -1,5 +1,6 @@
 import { AnimatePresence, motion } from "framer-motion";
 import { IoCloseOutline as Close } from "react-icons/io5";
+import { RiWhatsappFill } from "react-icons/ri";
 import React from "react";
 
 const ContactSideBar = (props) => {
@@ -16,10 +17,10 @@ const ContactSideBar = (props) => {
               x: "100%",
             }}
             transition={{ type: "spring", bounce: 0, duration: 0.8 }}
-            className="fixed bg-ijikod-dark-blue text-white shadow-lg top-0 right-0 w-1/3 h-screen flex flex-col gap-10 z-50"
+            className="fixed bg-ijikod-dark-blue text-white shadow-lg top-0 right-0 w-1/4 h-screen flex flex-col gap-10 z-50"
           >
             <div className="border-b border-gray-700 p-5 flex justify-between">
-              <h5>Contact Us</h5>
+              <h5 className="px-4">Contact Us</h5>
               <Close
                 className="cursor-pointer"
                 size={30}
@@ -27,19 +28,53 @@ const ContactSideBar = (props) => {
               />
             </div>
 
-            <div className="px-5">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi,
-              expedita.
+            <div className="px-10 text-xl">
+              Address
+              <h2 className="text-sm mt-3 font-light">
+                16 Ayuda Ritz Street Adenta, Accra — Ghana GS-012-3456
+                mail@ijikod.com +233 123 456 789
+              </h2>
+            </div>
+
+            <div className="px-10 text-xl">
+              Contact Us
+              <input
+                type="name"
+                id="name"
+                className="form"
+                placeholder="Name"
+                required
+              />
+              <input
+                type="email"
+                id="email"
+                className="form"
+                placeholder="Email"
+                required
+              />
+              <textarea
+                name="message"
+                id="message"
+                cols="10"
+                rows="10"
+                className="form"
+                placeholder="Message"
+              ></textarea>
+              <button className="textarea">Submit</button>
             </div>
 
             <div className="h-16 absolute bottom-0 left-0 right-0 border-t border-gray-700 flex items-center text-center">
               <div className="w-1/2 h-full border-r border-gray-700 pt-2">
-                <div>icon</div>
-                <div>WhatsApp</div>
+                <div className="flex flex-col item-center content-center">
+                  <div className="ml-20">
+                    <RiWhatsappFill />
+                  </div>
+                  WhatsApp us
+                </div>
               </div>
               <div className="w-1/2 h-full pt-2">
                 <div>icon</div>
-                <div>WhatsApp</div>
+                <div>Follow us on LinkedIn</div>
               </div>
             </div>
           </motion.div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,4 @@
-const {
-  screens
-} = require('tailwindcss/defaultTheme');
-
+const { screens } = require("tailwindcss/defaultTheme");
 
 module.exports = {
   content: [
@@ -10,89 +7,91 @@ module.exports = {
   ],
   theme: {
     screens: {
-      'xxxsm': '200px',
-      'xxsm': '340px',
-      'xsm': '480px',
-      ...screens
+      xxxsm: "200px",
+      xxsm: "340px",
+      xsm: "480px",
+      ...screens,
     },
 
     extend: {
-
       colors: {
-        'ijikod-dark': '#2A2E43',
-        'ijikod-gray-dark':'#0000000F',
+        "ijikod-dark": "#2A2E43",
+        "ijikod-gray-dark": "#0000000F",
         "ijikod-gray": "#7A7A7A",
-        "ijikod-dark-blue":"#2A2E43",
-        'ijikod-red': '#F40E41',
+        "ijikod-dark-blue": "#2A2E43",
+        "ijikod-red": "#F40E41",
         "ijikod-blue-deep": "#1438C9",
+        "ijikod-form-color": "#414557",
         "ijikod-blue-dark": "#011F3D",
         "ijikod-blue-light": "#1AA1DC",
-        'ijikod-wine': '#8F1742',
+        "ijikod-wine": "#8F1742",
         "ijikod-wine-dark-primary": "#62112E",
-        'ijikod-wine-dark': '#7D0032',
+        "ijikod-wine-dark": "#7D0032",
       },
 
       fontSize: {
-        'ijikod-xxxlg': '72px',
-        'ijikod-xxlg': '62px',
-        'ijikod-xlg': '52px',
-        'ijikod-lg': '44px',
+        "ijikod-xxxlg": "72px",
+        "ijikod-xxlg": "62px",
+        "ijikod-xlg": "52px",
+        "ijikod-lg": "44px",
 
-        'ijikod-xxmd': '38px',
-        'ijikod-xmd': '32px',
-        'ijikod-md': '28px',
+        "ijikod-xxmd": "38px",
+        "ijikod-xmd": "32px",
+        "ijikod-md": "28px",
 
-        'ijikod-sm-md': '24px',
+        "ijikod-sm-md": "24px",
 
-        'ijikod-sm': '16px',
-        'ijikod-xsm': '14px',
-        'ijikod-xxsm': '10px',
+        "ijikod-sm": "16px",
+        "ijikod-xsm": "14px",
+        "ijikod-xxsm": "10px",
       },
 
       minHeight: (theme) => ({
-        'h-min-screen': '100vh',
-        'ijikod-px-450': '450px',
-        ...theme('spacing'),
+        "h-min-screen": "100vh",
+        "ijikod-px-450": "450px",
+        ...theme("spacing"),
       }),
 
       minWidth: (theme) => ({
-        ...theme('spacing'),
+        ...theme("spacing"),
       }),
 
       height: {
-        'ijikod-h-nav': '15%',
-        'ijikod-h-hero': '85%',
-        '5/2': '40%',
-        'ijikod-px-80': '80px',
-        'ijikod-px-100': '100px',
-        'ijikod-px-120': '120px',
-        'ijikod-px-150': '150px',
-        'ijikod-px-40': '40px',
-        'ijikod-px-50': '50px',
-        'ijikod-px-450': '450px',
+        "ijikod-h-nav": "15%",
+        "ijikod-h-hero": "85%",
+        "5/2": "40%",
+        "ijikod-px-80": "80px",
+        "ijikod-px-100": "100px",
+        "ijikod-px-120": "120px",
+        "ijikod-px-150": "150px",
+        "ijikod-px-40": "40px",
+        "ijikod-px-50": "50px",
+        "ijikod-px-450": "450px",
       },
 
       inset: {
-        'ijikod-hero-offset-t': '15%',
+        "ijikod-hero-offset-t": "15%",
       },
 
       backgroundImage: {
-        'ijikod-hero-gradient': "linear-gradient(0deg, #8F1742 10.84%, #2A2E43 93.27%)",
+        "ijikod-hero-gradient":
+          "linear-gradient(0deg, #8F1742 10.84%, #2A2E43 93.27%)",
       },
 
       width: {
-        'ijikod-px-270': '270px',
-        'ijikod-px-124': '124px',
+        "ijikod-px-270": "270px",
+        "ijikod-px-400": "400px",
+        "ijikod-px-124": "124px",
       },
 
       borderRadius: {
-        'ijikod-px-20': '20px',
+        "ijikod-px-20": "20px",
       },
 
       fontFamily: {
-        'ijikod-open-sans': 'Open Sans'
-      }
+        "ijikod-open-sans": "Open Sans",
+      },
     },
   },
   plugins: [],
-}
+};
